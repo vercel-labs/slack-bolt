4fe19b7a959354a6c5dedf373a1126397123df0a
malewis5 <matthew.a.lewis7@gmail.com>
2025-07-18 11:48:51 -0400

    save

--- src/VercelReceiver.ts
+++ src/VercelReceiver.ts
@@ -237,25 +237,3 @@ export class VercelReceiver implements Receiver {
     };
   }
 }
-
-export function handler(app: App, receiver: VercelReceiver): VercelHandler {
-  let initPromise: Promise<void> | null = null;
-
-  return async (
-    req: VercelRequest,
-    res: VercelResponse
-  ): Promise<VercelResponse> => {
-    try {
-      if (!initPromise) {
-        initPromise = app.init();
-      }
-      await initPromise;
-
-      const handler = await receiver.start();
-      return await handler(req, res);
-    } catch (error) {
-      console.error("Error processing Slack event:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
